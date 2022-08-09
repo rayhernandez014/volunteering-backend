6fe1c10c4ef43ee79f0262464736cca7089c312c
Ray Hernandez <rayhernandez014@gmail.com>
2022-08-08 22:08:57 -0400

    uibuild

--- utils/config.js
+++ utils/config.js
@@ -8,15 +8,14 @@ const MONGODB_URI = process.env.MONGODB_URI
 const SECRET = process.env.SECRET
 
 const initializeRedis = () => {
-
-  if (process.env.REDISCLOUD_URL){
-    const redisClient = redis.createClient(process.env.REDISCLOUD_URL, { no_ready_check: true })
-    redisClient.on('error', (err) => console.log('Redis Client Error', err))
+  if (process.env.NODE_ENV === 'development') {
+    const redisClient = redis.createClient()
+    //redisClient.on('error', (err) => console.log('Redis Client Error', err))
     return redisClient
   }
-  else{
-    const redisClient = redis.createClient()
-    redisClient.on('error', (err) => console.log('Redis Client Error', err))
+  else if (process.env.NODE_ENV === 'production') {
+    const redisClient = redis.createClient(process.env.REDISCLOUD_URL, { no_ready_check: true })
+    //redisClient.on('error', (err) => console.log('Redis Client Error', err))
     return redisClient
   }
 }
