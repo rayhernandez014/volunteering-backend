ada1d8ed07c1923dbc51c7554de644890619b873
Ray Hernandez <rayhernandez014@gmail.com>
2022-08-08 22:48:43 -0400

    fixed a bug on the connection to redis

--- utils/config.js
+++ utils/config.js
@@ -10,17 +10,18 @@ const SECRET = process.env.SECRET
 const initializeRedis = () => {
   if (process.env.NODE_ENV === 'development') {
     const redisClient = redis.createClient()
-    //redisClient.on('error', (err) => console.log('Redis Client Error', err))
+    redisClient.on('error', (err) => console.log('Redis Client Error', err))
     return redisClient
   }
   else if (process.env.NODE_ENV === 'production') {
-    const redisClient = redis.createClient(process.env.REDISCLOUD_URL, { no_ready_check: true })
-    //redisClient.on('error', (err) => console.log('Redis Client Error', err))
+    const redisClient = redis.createClient({ url: process.env.REDISCLOUD_URL })
+    redisClient.on('error', (err) => console.log('Redis Client Error', err))
     return redisClient
   }
 }
 
 const redisClient = initializeRedis()
+
 
 module.exports = {
   MONGODB_URI,
